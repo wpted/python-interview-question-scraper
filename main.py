e97d957752cced01a12bad3a94b0d904b33eb6e1
wpted <silverhandsome@gmail.com>
2022-05-13 23:12:12 +0800

    Finalize the question list by removing unnecessary text.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ result.raise_for_status()
 soup = BeautifulSoup(result.text, 'html.parser')  # Soup
 tag_h3_list = soup.find_all("h3")  # List with elements of Type 'bs4.element.Tag'
 question_list = [str(question.get_text()) for question in tag_h3_list[3: len(tag_h3_list) - 1]]  # List
+question_list = [question for question in question_list if question[0].isdigit()]  # Remove bad categorized <h3> text
 
 
 def main():
