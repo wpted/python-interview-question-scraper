c9f2bf7cbbcb0445a4642364cfa1771b196c0793
wpted <silverhandsome@gmail.com>
2022-05-15 07:17:52 +0800

    Write questions to PDF, add Arial-Font Folder.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from fpdf import FPDF
 
 interview_question_url = "https://www.interviewbit.com/python-interview-questions/"
 result = requests.get(url=interview_question_url)
@@ -11,10 +12,15 @@ question_list = [str(question.get_text()) for question in tag_h3_list[3: len(tag
 question_list = [question for question in question_list if question[0].isdigit()]  # Remove bad categorized <h3> text
 
 
-def main():
-    for question in question_list:
-        print(question)
+
+pdf = FPDF()
+pdf.add_font("Arial", "", "arial/arial.ttf", uni=True)
+pdf.set_font("Arial", size=15)
+
+for question in question_list:
+    pdf.add_page()
+    pdf.multi_cell(0, 5, txt=question, align="L")
+
+pdf.output("Python-Interview-Questions.pdf").encode('latin-1', 'ignore')
 
 
-if __name__ == '__main__':
-    main()
